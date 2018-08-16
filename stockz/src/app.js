9efc0c7c02b57cbe0397c1939463cd819ed4097f
Adam Bien <abien@adam-bien.com>
2018-08-16 19:42:29 +0200

    50_custom_navigation_component

--- stockz/src/app.js
+++ stockz/src/app.js
@@ -1,4 +1,26 @@
-console.log("Hello Web Standards");
+class AirNav extends HTMLElement { 
+    constructor() { 
+        super();
+    }
+
+    connectedCallback() { 
+        const links = this.querySelectorAll("a");
+        console.log(links);
+        links.forEach(e => this.registerListener(e));
+    }
+
+    registerListener(e) { 
+        e.onclick = evt => this.onLinkClicked(evt);
+    }
+
+    onLinkClicked(evt) { 
+        evt.preventDefault();
+        console.log(evt);
+    }
+
+}
+
+customElements.define('air-nav',AirNav);
 
 
 
