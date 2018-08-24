8be7dd03afe75846f8e916a30791a29b7689ceab
Adam Bien <abien@adam-bien.com>
2018-08-24 19:21:37 +0200

    290_testing_hash_routing

--- stockz/test/navigation.spec.js
+++ stockz/test/navigation.spec.js
@@ -5,24 +5,35 @@ context('navigation', () => {
 
     const views = ["Overview", "Add", "List", "About"];
     views.forEach(view => {
-        it(`navigation by click -> ${view}`, () => {
+        it(`nav by click -> ${view}`, () => {
             cy.get(`[href="#${view}"]`).
                 click().should("have.class", "a-link");
-            cy.get('air-crumb').should('contain', view);
-            cy.get('air-slot').then(ref => { 
-                cy.window().then((win) => { 
-                    win.customElements.whenDefined('air-slot').
-                        then(() => {
-                            const { currentView } = ref[0];
-                            console.log("Upgraded->",currentView);
-                            expect(currentView).to.eq(view);
-            
-                    });
+            verify(view);
+        });
+    });
+
+    views.forEach(view => {
+        it (`nav by hash -> ${view}`, () => {
+            cy.visit(`#${view}`);
+            cy.get(`[href="#${view}"]`).should("have.class", "a-link");
+            verify(view)
+        });
+    
+    });
+
+    function verify(view) { 
+        cy.get('air-crumb').should('contain', view);
+        cy.get('air-slot').then(ref => { 
+            cy.window().then((win) => { 
+                win.customElements.whenDefined('air-slot').
+                    then(() => {
+                        const { currentView } = ref[0];
+                        console.log("Upgraded->",currentView);
+                        expect(currentView).to.eq(view);
+        
                 });
             });
         });
-    
 
-    });
-
+    }
 });
