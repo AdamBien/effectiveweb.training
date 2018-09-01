9d0a0a2438d687e718b3d4e86efa131e3ee843f3
Adam Bien <abien@adam-bien.com>
2018-09-01 16:46:38 +0200

    380_connecting_business_logic_with_ui

--- stockz/src/views/AddView.js
+++ stockz/src/views/AddView.js
@@ -1,13 +1,29 @@
+import Stocks from './Stocks.js';
+
 export default class AddView extends HTMLElement { 
     constructor() { 
         super();
+        this.root = this.attachShadow({mode:'open'});
     }
 
     connectedCallback() { 
-        this.innerHTML = `
-            <input placeholder="stock"/>
+        this.root.innerHTML = `
+            <input id="name" placeholder="name"/>
+            <input id="price" placeholder="price"/>
+            <input id="amount" placeholder="amount"/>
             <button>add</button>
-        `
+        `;
+        this.nameInput = this.root.querySelector('#name');
+        this.priceInput = this.root.querySelector('#price');
+        this.amountInput = this.root.querySelector('#amount');
+        this.root.querySelector('button').onclick = _ => this.addStock();
+    }
+
+    addStock() { 
+        const name = this.nameInput.value;
+        const price = this.priceInput.value;
+        const amount = this.amountInput.value;
+        Stocks.add(name,price,amount);
     }
 
 }
