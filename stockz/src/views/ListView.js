ee9a60b29922ddbedbd26f42f0d1775987d4fe9d
Adam Bien <abien@adam-bien.com>
2018-09-02 17:49:50 +0200

    450_deleteting_rows_in_table

--- stockz/src/views/ListView.js
+++ stockz/src/views/ListView.js
@@ -3,6 +3,7 @@ export default class ListView extends HTMLElement {
 
     constructor() { 
         super();
+        this.root = this.attachShadow({mode:'open'});
     }
 
     connectedCallback() { 
@@ -11,7 +12,7 @@ export default class ListView extends HTMLElement {
     }
 
     render() { 
-        this.innerHTML = `
+        this.root.innerHTML = `
         <style>
          header{
              background: var(--air-brown, red);
@@ -21,7 +22,8 @@ export default class ListView extends HTMLElement {
             <h2>the stocks</h2>
             </header>
             ${this.table()}
-        `
+        `;
+        this.root.querySelectorAll('button').forEach(button => button.onclick = e => this.removeStock(e));
     }
     table() { 
         return `
@@ -47,9 +49,13 @@ export default class ListView extends HTMLElement {
     row({name,price,amount}) { 
         return `
         <tr>
-        <td>${name}</td><td>${price}</td><td>${amount}</td>
+        <td>${name}</td><td>${price}</td><td>${amount}</td><td><button id="${name}">remove</button></td>
         </tr>
         `;
+    }
+
+    removeStock({ target }) { 
+        Stocks.remove(target.id);
     }
 
 
