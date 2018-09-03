5b3ebde4801731d540f177113566a68a16dc22de
Adam Bien <abien@adam-bien.com>
2018-09-03 14:46:10 +0200

    490_creating_a_bar_chart_with_d3

--- stockz/src/views/Overview.js
+++ stockz/src/views/Overview.js
@@ -1,12 +1,40 @@
+import Stocks from './Stocks.js';
 export default class OverviewView extends HTMLElement{ 
     constructor(){ 
         super();
+        this.root = this.attachShadow({mode:'open'});
     }
 
-    connectedCallback(){ 
-        this.innerHTML = `
-            <output>10000 SUN Micro</output>
+    connectedCallback() { 
+        this.render();
+        addEventListener('air-stocks',e=>this.render());
+    }
+
+    render(){ 
+        this.root.innerHTML = `
+        <style>
+        .chart div {
+          font: 10px sans-serif;
+          background-color: steelblue;
+          text-align: right;
+          padding: 3px;
+          margin: 1px;
+          color: white;
+        }
+        
+        </style>
+        <div class="chart">
+        </div>
         `;
+        const data = Stocks.all();
+        const chartNode = this.root.querySelector('.chart');
+        d3.select(chartNode)
+        .selectAll("div")
+          .data(data)
+            .enter().
+            append("div")
+          .style("width",  stock => stock.total * 10 + "px")
+          .text(stock => `${stock.name}: ${stock.total}`);        
     }
 }
 customElements.define('overview-view',OverviewView);
