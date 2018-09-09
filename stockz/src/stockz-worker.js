48e28d8a15cc631b6e946633213d530df5002a92
Adam Bien <abien@adam-bien.com>
2018-09-09 17:54:20 +0200

    590_onfetch_in_service_worker

--- stockz/src/stockz-worker.js
+++ stockz/src/stockz-worker.js
@@ -1,10 +1,16 @@
 
 
 const cacheName = 'stockz-cache';
-const resources = ['index.html','style.css','app.js','AirNav.js','AirSlot.js','AirCrumb.js','views/TotalView.js','views/AirElements.js','views/Stocks.js'];
+const resources = ['index.html','style.css','configuration.json','app.js','AirNav.js','AirSlot.js','AirCrumb.js','views/TotalView.js','views/AirElements.js','views/Stocks.js'];
 console.log('caches', caches);
 
 
 self.addEventListener('install', event =>
-    caches.open(cacheName).then(cache => cache.addAll(resources))
+    event.waitUntil(caches.open(cacheName).then(cache => cache.addAll(resources)))
 )
+
+self.addEventListener('fetch', event => {
+    const { request } = event;
+    console.log('onfetch ', request);
+    event.respondWith(caches.match(request).then(response => (response || fetch(request))))
+});
