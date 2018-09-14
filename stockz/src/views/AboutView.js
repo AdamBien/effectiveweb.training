68f71502a6d8ff5ea379f3978cdc026283b4edf4
Adam Bien <abien@adam-bien.com>
2018-09-14 20:41:52 +0200

    730_string_to_custom_element_migration

--- stockz/src/views/AboutView.js
+++ stockz/src/views/AboutView.js
@@ -1,8 +1,27 @@
-const name = "duke";
-const content = `
-<article>
- <h3>powered by web standards && ${name}</h3>
-</article>
-`
+import AirElement from "./AirElement.js";
+import { html } from "../lit-html/lit-html.js";
 
-export default content;
+export default class AboutView extends AirElement { 
+
+    constructor() { 
+        super();
+        this.name = "duke";
+    }
+    connectedCallback() { 
+        this.viewChanged();
+    }
+
+    createView() { 
+        return html`
+        <article>
+         <h3>powered by web standards && ${name}</h3>
+        </article>
+        `;
+
+    }
+}
+
+customElements.define('about-view',AboutView);
+
+
+
